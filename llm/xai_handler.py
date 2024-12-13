d118714ab6d08289537ddb8e82ad52a5401a11f2
Ashwin <ashwins.2019a@vitstudent.ac.in>
2024-12-13 20:32:38 +0530

    bug fixed

--- llm/xai_handler.py
+++ llm/xai_handler.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 from openai import OpenAI
 import json
 import logging
+import base64
 
 from langchain_openai import ChatOpenAI
 from langchain_core.utils.function_calling import convert_to_openai_function
